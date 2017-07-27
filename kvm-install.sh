3511f4a056d82dfd9f9b882e8e5167c855a083e8
Harrison McCullough <harrison_mccullough@labs.att.com>
2017-07-27 15:27:05 -0500

    Add kvm-install script

---
+++ kvm-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://www.linux-kvm.org/page/RunningKVM
+
+packages=(gcc
+          libsdl1.2-dev
+          zlib1g-dev
+          libasound2-dev
+          linux-kernel-headers
+          pkg-config
+          libgnutls-dev
+          libpci-dev)
+
+sudo apt-get install "${packages[@]}"
